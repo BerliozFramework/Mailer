9c33297d42a8717c30d2300e23164f6a7bdb5979
Ronan Giron <ElGigi@users.noreply.github.com>
2017-09-13 17:06:37 +0200

    Add option to get minified HTML string from Mail class

--- src/Mail.php
+++ src/Mail.php
@@ -313,11 +313,50 @@ class Mail
     /**
      * Get html.
      *
+     * @param bool $minified (default: false)
+     *
      * @return string
      */
-    public function getHtml(): string
+    public function getHtml(bool $minified = false): string
     {
-        return $this->html;
+        if ($minified) {
+            // Save and change PHP configuration value
+            $oldPcreRecursionLimit = ini_get('pcre.recursion_limit');
+            if (PHP_OS == 'WIN') {
+                ini_set('pcre.recursion_limit', '524');
+            } else {
+                ini_set('pcre.recursion_limit', '16777');
+            }
+
+            $regex = <<<EOT
+%# Collapse whitespace everywhere but in blacklisted elements.
+(?>             # Match all whitespans other than single space.
+  [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+| \s{2,}        # or two or more consecutive-any-whitespace.
+) # Note: The remaining regex consumes no text at all...
+(?=             # Ensure we are not in a blacklist tag.
+  [^<]*+        # Either zero or more non-"<" {normal*}
+  (?:           # Begin {(special normal*)*} construct
+    <           # or a < starting a non-blacklist tag.
+    (?!/?(?:textarea|pre|script)\b)
+    [^<]*+      # more non-"<" {normal*}
+  )*+           # Finish "unrolling-the-loop"
+  (?:           # Begin alternation group.
+    <           # Either a blacklist start tag.
+    (?>textarea|pre|script)\b
+  | \z          # or end of file.
+  )             # End alternation group.
+)  # If we made it here, we are not in a blacklist tag.
+%Six
+EOT;
+
+            // Reset PHP configuration value
+            ini_set('pcre.recursion_limit', $oldPcreRecursionLimit);
+
+            return preg_replace($regex, ' ', $this->html);
+        } else {
+            return $this->html;
+        }
     }
 
     /**
